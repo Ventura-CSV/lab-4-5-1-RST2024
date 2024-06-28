4c5ba8b9dc37bcd8e8f3a8596ea5df15118df187
RST2024 <rtanwani2024@gmail.com>
2024-06-27 22:49:18 -0700

    msg

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ def main():
     Code Your Program here
     ########################################
     """
+   
+    i = 0
+    total = 0
+    numbers = [0] * 5
+    print(numbers)
+    while i < 5:
+        number = random.randint(0,100)
+        numbers[i] = number
+        total = total + number
+        i = i + 1
+        
+    
+    
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
